6fa9b04141def5280507af3dbe0f5aea5195dd96
Rohail <rmsyed@umich.edu>
2018-03-17 17:10:37 -0400

    Updated wiki_parser_3.py to include alternative method of computing document frequencies

--- wiki_parser_3.py
+++ wiki_parser_3.py
@@ -16,6 +16,86 @@ from gensim.corpora.wikicorpus import extract_pages, process_article
 
 
 
+
+# protects against invalid unicode characters
+def rectify(selstr):
+	val = ''.join([x for x in selstr if ord(x)<128])
+	return str(val)
+
+
+
+stemmer = PorterStemmer()
+stops = set(stopwords.words("english"))
+
+def tokenizer(text, token_min_len,token_max_len, lower):
+	fullsents = nltk.sent_tokenize(text)
+	fullseqs = []
+	for sent in fullsents:
+		fullseqs += [rectify(term.lower()) for term in nltk.word_tokenize(sent) if ((term not in stops) and term.isalnum())]
+	fullseqs_stemmed = [stemmer.stem(term) for term in fullseqs] #... keep both the stemmed and unstemmed versions
+	return [fullseqs, fullseqs_stemmed]
+
+
+def loadSimpleWiki():
+	#file_name = "C:/Users/Admin/Anaconda2/envs/py27/corpora/wiki2017/simplewiki-20170820-pages-meta-current.xml.bz2"
+	#counter  =0
+	#... 179,620 articles found
+	file_name = "C:/Users/Admin/Anaconda2/envs/py27/corpora/wiki2017/simplewiki-20170820-pages-meta-current.xml.bz2"
+
+	wiki = WikiCorpus(file_name, lemmatize=False, dictionary={}) #vocab dict not needed
+	i = 0
+	#for text in wiki.get_texts():
+	allwords = defaultdict(lambda:0)
+	allsents = []
+	print ("starting...")
+	dfcounter_stemmed = defaultdict(lambda: 0)
+	dfcounter_nostem = defaultdict(lambda: 0)
+
+	for (title,article,pageid) in extract_pages(bz2.BZ2File(file_name), filter_namespaces=('0', )): #filter_namespaces=["0"]):
+		if len(article)==0:
+			continue
+
+		text = process_article((filter_wiki(article), False, title, pageid), tokenizer_func=tokenizer)
+		text_orig = set(text[0][0]) #... We are ONLY interested in whether or not the term appeared in this document NOT how many times or where
+		text_stemmed = set(text[0][1])
+
+		for term in text_orig:
+			dfcounter_nostem[term] += 1
+		for term in text_stemmed:
+			dfcounter_stemmed[term] += 1
+
+		i+=1
+		if i%1000==0:
+			print (i)
+			#break
+	print (i)
+
+	handle = open("simplewiki_docfreqs_stemmed.txt","w+")
+	for key,val in dfcounter_stemmed.items():
+		handle.write(str(key)+"\t"+str(val)+"\n")
+	handle.close()
+
+	handle = open("simplewiki_docfreqs_nostem.txt","w+")
+	for key,val in dfcounter_nostem.items():
+		handle.write(str(key)+"\t"+str(val)+"\n")
+	handle.close()
+
+
+if __name__ == "__main__":
+	loadSimpleWiki()
+	sys.exit()
+
+
+
+
+
+
+
+
+
+
+#........... Get document frequencies from the BOW files generated through the gensim script...........
+#.......................................................................................................
 def documentFrequencies():
 	dictionary = Dictionary.load_from_text('C:/Users/Admin/Anaconda2/envs/py27/corpora/wiki2017_wordids.txt.bz2')
 	print (max(dictionary.token2id.values()))
@@ -67,4 +147,4 @@ def documentFrequencies():
 		print (featcount)
 
 
-documentFrequencies()
+#documentFrequencies()
